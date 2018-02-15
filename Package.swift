e9c45e2552ca460dcce18bc6a9851fa10b1513d7
Pascal Morillon <pascal.morillon@irisa.fr>
2018-02-15 09:05:22 +0100

    Add Console dependency and fixes Postgres dependency

--- Package.swift
+++ Package.swift
@@ -6,13 +6,14 @@ import PackageDescription
 let package = Package(
     name: "swift_oar_es_indexer",
     dependencies: [
-        .package(url: "https://github.com/vapor/postgresql.git", from: "2.1.1"),
+        .package(url: "https://github.com/vapor-community/postgresql.git", from: "2.1.1"),
+        .package(url: "https://github.com/vapor/console.git", from: "2.3.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "swift_oar_es_indexer",
-            dependencies: ["PostgreSQL"]),
+            dependencies: ["PostgreSQL", "Console"]),
     ]
 )
